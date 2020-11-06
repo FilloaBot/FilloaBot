c7646efdedde500475cbfe34853195f0816e5e51
Bane34 <bane34.42@gmail.com>
2020-11-06 18:22:36 +0100

    " "

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 import json
+import asyncio
 import discord
-from discord.ext import commands
-import random
-
+import random, time
+from discord.ext import commands, tasks
+from discord.ext.commands import Bot
 from config.variables import *
 
 #load the bot toke from the json file
 with open("token.json") as file:
     data = json.load(file)
-
 token = data['token']
 
+intents = discord.Intents.default()
+intents.members = True
+intents.reactions = True
+intents.messages = True
+intents.emojis = True
 bot = commands.Bot(command_prefix = '?', description = "Bot para diversos propositos")
+
+@tasks.loop(seconds=25)
+async def comprobar_pechi():
+    channel = await bot.get_channel(channel.user.id) 
+    await channel.send("A pechi le gusta oliveira")
 
 @bot.command(
     name = "filloas",
@@ -42,15 +52,21 @@ async def aleatorio(ctx, num1: int, num2: int):
 async def twitch(ctx):
     await ctx.send("Visita mi canal de twitch: " + urls[3])
 
+
+
 @bot.event
 async def on_ready():
     game = discord.Game("Usa ? para invocar al filloa bot")
     await bot.change_presence(status = discord.Status.idle, activity = game)
     print("Filloa bot encendio")
+    print("\nLogged in as")
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
 
 @bot.listen()
 async def on_message(message):
-    if message.content.endswith("5"):
+    if message.content.endswith("5"):        
         await message.channel.send("Por el culo te la inco")
         await bot.process_commands(message)
 
