77578042e8e72a1d60ee10fb5804bbfd6d5336a9
Bane34 <bane34.42@gmail.com>
2020-11-08 14:23:32 +0100

    Added some tasks

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import discord
 import re
 from discord.ext import commands, tasks
 from discord.ext.commands import Bot
+from itertools import cycle
 
 from config.variables import *
 
@@ -18,6 +19,8 @@ intents.messages = True
 intents.emojis = True
 bot = commands.Bot(command_prefix = '?', description = "Bot para diversos propositos")
 
+status = cycle(status)
+
 #Comandos
 @bot.command(
     name = "filloas",
@@ -26,20 +29,25 @@ bot = commands.Bot(command_prefix = '?', description = "Bot para diversos propos
     pass_context = True
 )
 async def filloas(ctx):
-    number = random.randint(0, 2)
+    number = random.randint(0, 3)
     await ctx.send(urls[number])
+
+@tasks.loop(seconds = 60)
+async def change_status():
+    await bot.change_presence(activity = discord.Game(next(status)))
+    print("LOG: status changed")
 
 #Eventos
 @bot.event
 async def on_ready():
-    game = discord.Game("Usa ? para invocar al filloa bot")
-    await bot.change_presence(status = discord.Status.idle, activity = game)
+    change_status.start()
     print("Filloa bot encendio")
     print("\nLogged in as")
     print(bot.user.name)
     print(bot.user.id)
     print('------')
-
+    
+"""
 @bot.listen()
 async def on_message(message):
     if message.author == bot.user:
@@ -51,5 +59,6 @@ async def on_message(message):
             for reaction in reference["reactions"]:
                 await message.add_reaction(reaction)
             await bot.process_commands(message)
+"""
 
 bot.run(token)
