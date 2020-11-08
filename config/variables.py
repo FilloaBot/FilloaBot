1288b5bcbbd3b9d150e7eaa977408ab2791a22af
Bane34 <bane34.42@gmail.com>
2020-11-08 14:24:04 +0100

    Added status

--- config/variables.py
+++ config/variables.py
@@ -1,5 +1,13 @@
 urls = [
     "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fksr-ugc.imgix.net%2Fassets%2F012%2F392%2F159%2Fc6164f0ec2b18ec50a160f83a40ebb40_original.jpg%3Fw%3D1536%26h%3D864%26fit%3Dfill%26bg%3DC7C9C8%26v%3D1463758718%26auto%3Dformat%26q%3D92%26s%3D91ba9d0aec3f57d96d2756a2026c174d&f=1&nofb=1",
     "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fokdiario.com%2Fimg%2F2016%2F12%2F07%2Freceta-filloas-galicia-655x368.jpg&f=1&nofb=1",
-    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FS8gy1acgvWE%2Fmaxresdefault.jpg&f=1&nofb=1"
+    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FS8gy1acgvWE%2Fmaxresdefault.jpg&f=1&nofb=1",
+    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fsaborgourmet.com%2Fwp-content%2Fuploads%2Ffilloas-gallegas.jpg&f=1&nofb=1"
     ]
+
+status = [
+    "Usa ? para ejecutar un comando",
+    "En realidad alguien lee esto?",
+    "A quien no le gustan las filloas?",
+    "Ella no te quiere :("
+]
