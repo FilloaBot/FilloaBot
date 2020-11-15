1d1a377e0fc9c371d82005224c9232c78863b1b3
Bane34 <bane34.42@gmail.com>
2020-11-15 14:20:25 +0100

    started economy cog

---
+++ cogs/economy.py
@@ -0,0 +1,11 @@
+import discord
+from discord.ext import commands
+
+class Economy(commands.Cog):
+    """
+    The economy is going to be one of the main purposes of this bot
+    """
+
+    def __init__(self, bot):
+        self.bot = bot
+
