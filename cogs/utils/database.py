2f217cfb3f1e7f0d2623e27dfb55bfe757da1ea6
Bane34 <bane34.42@gmail.com>
2020-11-22 14:37:01 +0100

    added database handling

--- cogs/utils/database.py
+++ cogs/utils/database.py
@@ -1,25 +1,5 @@
 import sqlite3
 from sqlite3 import Error
-"""
-def sql_connection():
-    try:
-        con = sqlite3.connect("database.db")
-        return con
-    except Error:
-        print(Error)
-
-def sql_table(con):
-    cursorObj = con.cursor()
-    cursorObj.execute("CREATE TABLE empleados(id integer PRIMARY KEY, name text, salary real, department text, position text, hireDate text)")
-
-def sql_update(con):
-    cursorObj = con.cursor()
-    cursorObj.execute('UPDATE empleados SET name = "Rogers" where id = 2')
-    con.commit()
-
-con = sql_connection()
-sql_update(con)
-"""
 
 class main_db():
     def __init__(self, database):
@@ -39,11 +19,13 @@ class main_db():
         s = (user_name,)
         cursorObj.execute("SELECT amount FROM balance WHERE user = ?", s)
 
-#        for row in cursorObj.fetchall():
-#           amount = row
-        return cursorObj.fetchall()[0][0]
+        if not cursorObj.fetchall():
+            print("El usuario no existe melon")
+            return None
 
-    def add_user_balance(self, user_name, balance):
+        return cursorObj.fetchall()
+
+    def add_user_balance(self, user_name, balance = 0):
         con = sqlite3.connect(self.database)
         cursorObj = con.cursor()
         s = (user_name, balance,)
