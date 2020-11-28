388f5c97bc44d961d0e15136a43dea36fb4e5ebb
Pedro Rey Anca <pedroreya@gmail.com>
2020-11-28 11:43:11 +0100

    voice downloads from youtube url in msg

--- cogs/voice.py
+++ cogs/voice.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 from discord.utils import get
 from discord import FFmpegPCMAudio
+import youtube_dl 
+import os
 
 class Voice(commands.Cog):
     def __init__(self, bot):
@@ -16,21 +18,20 @@ class Voice(commands.Cog):
         channel = ctx.message.author.voice.channel
         voice = get(self.bot.voice_clients, guild = ctx.guild)
         
-        if voice and voice.is_connected():
+        if not voice == None:
             await voice.move_to(channel)
         else:
             voice = await channel.connect()
 
         await ctx.guild.change_voice_state(channel=channel, self_deaf=True)
-        player = FFmpegPCMAudio("fuentedepoder.mp3")
-        voice.play(player)
+        
 
     @commands.command()
     async def leave(self, ctx):
-        channel = ctx.message.voice.channel
+        channel = ctx.message.author.voice.channel
         voice = get(self.bot.voice_clients, guild = ctx.guild)
 
-        if voice and voice.is_connected():
+        if not voice == None:
             await voice.disconnect()
             await ctx.send(f"Desconectado del canal {channel}")
         else:
@@ -43,9 +44,39 @@ class Voice(commands.Cog):
             return
 
         channel = ctx.message.author.voice.channel
-        voice = get(self.bot.voice_clients, guild = ctx)
-        
-        if voice and voice.is_connected():
+        voice = get(self.bot.voice_clients, guild = ctx.guild)
+
+        if not voice == None:
             await voice.move_to(channel)
         else:
-            voice = await voice.connect()
+            voice = await channel.connect()
+
+        await ctx.guild.change_voice_state(channel=channel, self_deaf=True)
+
+        await ctx.send("Comenzando descarga...")
+
+        filePath = str(ctx.guild.id) + ".mp3"
+
+        if os.path.exists(filePath):
+            os.remove(filePath)
+
+        ydl_opts = {
+            'outtmpl': filePath,
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }]
+        }
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+
+        await ctx.send("Descarga finalizada. Reproduciendo.")
+
+        player = FFmpegPCMAudio(filePath)
+        if voice.is_playing():
+            voice.stop()
+        
+        voice.play(player)
+        
