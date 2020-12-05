5ea551d8651083e4278c6b0801636185b77d3383
Pedro Rey Anca <pedroreya@gmail.com>
2020-12-05 02:25:22 +0100

    Added queue to voice

--- cogs/utils/database.py
+++ cogs/utils/database.py
@@ -46,3 +46,116 @@ class main_db():
             cursorObj.execute("UPDATE balance SET amount = ? WHERE user = ?", s)
 
         con.commit()
+
+    ## Starts part for the music cog
+    def queue_exist(self, guildId):
+        con = sqlite3.connect(self.database)
+        cursorObj = con.cursor()
+        s = (guildId,)
+        cursorObj.execute("SELECT guildId FROM musicQueues WHERE guildId = ?", s)
+
+        con.commit()
+        return not (len(cursorObj.fetchall()) == 0)
+
+    def get_queue(self, guildId):
+        con = sqlite3.connect(self.database)
+        cursorObj = con.cursor()
+        if not self.queue_exist(guildId):
+            return None
+        s = (guildId,)
+
+        cursorObj.execute("SELECT * FROM musicQueues WHERE guildId = ?", s)
+        out = cursorObj.fetchall()[0]
+        if out[3] == 0:
+            loopPos = None
+        else:
+            loopPos -= 1
+        data = {
+            "guildId": out[0],
+            "queue": out[1].split(";;;"),
+            "loop": bool(out[2]),
+            "loopPos": out[3],
+            "shuffle": bool(out[4]),
+            "currentPos": out[5] or 0
+        }
+
+        return data
+    def modify_queue(self, guildId, queue=None, loop=False, loopPos=None, shuffle=False, currentPos=None):
+        if loopPos == None:
+            loopPos = 0
+        if queue == None:
+            if self.get_queue(guildId) == None:
+                queue = None
+            else:
+                queue = self.get_queue(guildId)["queue"]
+
+        queue = ";;;".join(queue)
+        loop = int(loop)
+        shuffle = int(shuffle)
+
+        con = sqlite3.connect(self.database)
+        cursorObj = con.cursor()
+
+        if self.queue_exist(guildId):
+            s = (queue, loop, loopPos, shuffle, currentPos, guildId,)
+            cursorObj.execute("UPDATE musicQueues SET queue = ?, loop = ?, loopPos = ?, shuffle = ?, currentPos = ? WHERE guildId = ?", s)
+        else:
+            s = (guildId, queue, loop, loopPos, shuffle, currentPos,)
+            cursorObj.execute("INSERT INTO musicQueues VALUES(?,?,?,?,?,?)", s)
+        con.commit()
+
+    def insert_into_queue(self, guildId, ytId):
+        queue = self.get_queue(guildId)
+        if queue == None:
+            queue = []
+        else:
+            queue = queue["queue"]
+        queue.append(ytId)
+        self.modify_queue(guildId, queue=queue)
+        return queue
+    
+    def remove_from_queue(self, guildId, delPos):
+        queue = self.get_queue(guildId)
+        if queue == None:
+            queue = []
+        else:
+            queue = queue["queue"]
+        del queue[delPos]
+        self.modify_queue(guildId, queue=queue)
+        return queue
+
+    def clear_queue(self, guildId):
+        con = sqlite3.connect(self.database)
+        cursorObj = con.cursor()
+
+        s = (guildId,)
+        cursorObj.execute("DELETE FROM musicQueues WHERE guildId = ?", s)
+        
+        con.commit()
+
+    def increment_current_pos(self, guildId):
+        queue = self.get_queue(guildId)
+        if queue == None:
+            currentPos = -1
+        else:
+            currentPos = queue["currentPos"]
+            currentPos += 1
+            if currentPos >= len(queue["queue"]):
+                currentPos = 0
+        self.modify_queue(guildId, currentPos=currentPos)
+        return currentPos
+
+    def decrement_current_pos(self, guildId):
+        queue = self.get_queue(guildId)
+        if queue == None:
+            currentPos = -1
+        else:
+            currentPos = queue["currentPos"]
+            currentPos -= 1
+            if currentPos < 0:
+                currentPos = len(queue["queue"]) -1
+        self.modify_queue(guildId, currentPos=currentPos)
+        print(currentPos)
+        return currentPos
+
+    
--- cogs/voice.py
+++ cogs/voice.py
@@ -8,12 +8,14 @@ from discord.ext import commands
 from discord.utils import get
 from discord import FFmpegPCMAudio, Embed, Color
 
-queues = {}
+from cogs.utils.database import *
+
+database = main_db("./database.db")
 
 class Voice(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
+
     @commands.command()
     async def join(self, ctx):
         global voice
@@ -42,7 +44,9 @@ class Voice(commands.Cog):
             await ctx.send("No estoy conectado a ningun canal fetido")
 
     @commands.command()
-    async def play(self, ctx, url: str):
+    async def play(self, ctx, url: str, noQueue=False):
+        if not noQueue == True:
+            noQueue = False
         if ctx.author.voice == None:
             await ctx.send("No estas en un canal de voz melon")
             return
@@ -62,55 +66,97 @@ class Voice(commands.Cog):
         
         await ctx.guild.change_voice_state(channel = channel, self_deaf = True)
 
-        filePath = str(ctx.guild.id) + ".mp3"
-
-        if os.path.exists(filePath):
-            os.remove(filePath)
-
         ydl_opts = {
-            'default_search': 'auto',
-            'outtmpl': filePath,
-            'format': 'bestaudio/best',
-            'postprocessors': [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '192',
-            }]
+            'default_search': 'auto'
         }
+
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([url])
             info_dict = ydl.extract_info(url, download=False)
-            info_dict = info_dict["entries"][0]
+            try:
+                info_dict = info_dict["entries"][0]
+            except Exception as e:
+                pass
             video_id = info_dict.get("id", None)
             video_url = "https://youtube.com/watch?v=" + video_id
             video_title = info_dict.get('title', None)
+        if not noQueue:
+            database.insert_into_queue(ctx.guild.id, video_id)
+        url = video_id
 
-        embed = Embed(
-            title = "**Reproduciendo**",
-            description = f"[{video_title}]({video_url}) [{ctx.message.author.mention}]",
-            colour = Color(0x3A425D),
-        )
-        await ctx.send(embed = embed)
-
-        player = FFmpegPCMAudio(filePath)
-        if voice.is_playing():
-            voice.stop()
-        
-        voice.play(player)
+        if voice == None or not voice.is_playing():
+            filePath = str(ctx.guild.id) + ".mp3"
+            if os.path.exists(filePath):
+                os.remove(filePath)
+            ydl_opts = {
+                'outtmpl': filePath,
+                'format': 'bestaudio/best',
+                'postprocessors': [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3',
+                    'preferredquality': '192',
+                }]
+            }
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([url])
+            embed = Embed(
+                title = "**Reproduciendo**",
+                description = f"[{video_title}]({video_url}) [{ctx.message.author.mention}]",
+                colour = Color(0x3A425D),
+            )
+            await ctx.send(embed = embed)
+            player = FFmpegPCMAudio(filePath)
+            if voice.is_playing():
+                voice.stop()
+            voice.play(player)
+        else:
+            embed = Embed(
+                title = "**Añadido a la cola**",
+                description = f"[{video_title}]({video_url}) [{ctx.message.author.mention}]",
+                colour = Color(0x3A425D),
+            )
+            await ctx.send(embed = embed)
         
     @commands.command()
-    async def pause(self, ctx):
-        emoji = '⏸️'
+    async def next(self, ctx):
+        emoji = '⏭️'
         msg = ctx.message
         await msg.add_reaction(emoji)
-
         voice = get(self.bot.voice_clients, guild = ctx.guild)
+ 
+        nextYtId = database.get_queue(ctx.guild.id)["queue"][database.increment_current_pos(ctx.guild.id)]
+        pending_command = self.bot.get_command("play")
+        if not voice == None:
+            voice.stop()
+        await ctx.invoke(pending_command, nextYtId, True)
 
-        if voice and voice.is_playing():
-            voice.pause()
-        #     await ctx.send("Pausando reproduccion")
-        # else:
-        #     await ctx.send("No estas reproduciendo nada ahora mismo")
+    @commands.command()
+    async def previous(self, ctx):
+        emoji = '⏮️'
+        msg = ctx.message
+        await msg.add_reaction(emoji)
+        voice = get(self.bot.voice_clients, guild = ctx.guild)
+ 
+        nextYtId = database.get_queue(ctx.guild.id)["queue"][database.decrement_current_pos(ctx.guild.id)]
+        pending_command = self.bot.get_command("play")
+        if not voice == None:
+            voice.stop()
+        await ctx.invoke(pending_command, nextYtId, True)
+
+    @commands.command()
+    async def clear(self, ctx):
+        emoji = '⬜'
+        msg = ctx.message
+        await msg.add_reaction(emoji)
+        voice = get(self.bot.voice_clients, guild = ctx.guild)
+ 
+        database.clear_queue(ctx.guild.id)
+        if not voice == None:
+            voice.stop()
+        embed = Embed(
+                title = "**Cola vaciada**",
+                colour = Color(0x3A425D),
+            )
+        await ctx.send(embed=embed)
             
     @commands.command()
     async def resume(self, ctx):
@@ -120,17 +166,8 @@ class Voice(commands.Cog):
 
         voice = get(self.bot.voice_clients, guild = ctx.guild)
 
-        if not voice == None:
-            if voice.is_paused():
+        if not voice == None and voice.is_paused():
                 voice.resume()
-        #         await ctx.send("Resumiendo reproduccion")
-        #     elif voice.is_playing():
-        #         await ctx.send("Ya se esta reproduciendo, no se puede resumir melon")
-        #     else:
-        #         await ctx.send("No se esta reproduciendo nada, no se puede resumir melon")
-        # else:
-        #     await ctx.send("No se esta reproduciendo nada, no se puede resumir melon")
-
     @commands.command()
     async def stop(self, ctx):
         emoji = '🛑'
@@ -140,42 +177,5 @@ class Voice(commands.Cog):
         voice = get(self.bot.voice_clients, guild = ctx.guild)
 
         if not voice == None:
-            # await ctx.send("Parando de reproducir")
             voice.stop()
-        # else:
-        #     await ctx.send("No se esta reproduciendo nada, no se puede parar melon")
-
-    @commands.command()
-    async def queue(self, ctx, url: str):
-        Queue_infile = os.path.isdir("./Queue")
-
-        if Queue_infile is False:
-            os.mkdir("Queue")
-
-        DIR = os.path.abspath(os.path.realpath("Queue"))
-        q_num = len(os.listdir(DIR))
-        q_num += 1
-        add_queue = True
-        while add_queue:
-            if q_num in queues:
-                q_num += 1
-            else:
-                add_queue = False
-                queues[q_num] = q_num
         
-        queue_path = os.path.abspath(os.path.realpath("Queue"))
-
-        ydl_opts = {
-            'default_search': 'auto',
-            'format': 'bestaudio/best',
-            'postprocessors': [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '192',
-            }]
-        }
-
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([url])
-
-        await ctx.send(f"Añadiendo la cancion {str(q_num)} a la cola")
